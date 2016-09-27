3285d03a7c0ad1bdf788ddb4304337e8d5ada619
Gkxd <gkxd@users.noreply.github.com>
2016-09-26 20:31:29 -0400

    Added in game height calibration

--- Assets/Scripts/OculusCalibration.cs
+++ Assets/Scripts/OculusCalibration.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OculusCalibration : MonoBehaviour {
+public class OculusCalibration : MonoBehaviour
+{
+    
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
+    }
 }
